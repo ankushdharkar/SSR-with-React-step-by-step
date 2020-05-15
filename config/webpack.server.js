8e09099165a20c5329990f40f27bdb3cf452f9e5
ankushdharkar <ankushdharkar@gmail.com>
2020-05-15 13:51:30 +0530

    Run webpack babel for server and use build

---
+++ config/webpack.server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',  // Server side. Not for client browser
+  entry: './server/index.js',
+  output : {
+    filename: 's-bundle.js',
+    path: path.resolve(__dirname, '../build')  
+  },
+  module: {
+    rules: [
+      {
+        test:/\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            '@babel/react',
+            ['@babel/env', { targets: { browsers: ['last 2 versions'] } } ]
+          ]
+        }
+      }
+    ]
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const React = require('react');
 const renderToString = require('react-dom/server').renderToString;
 
 const AppComponent = require('./../src/App').default;
