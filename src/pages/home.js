d7b500d58a57fc7b3422b4952740573ef27d3e7f
ankushdharkar <ankushdharkar@gmail.com>
2020-05-16 01:28:09 +0530

    Helmet on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function Home() {
+  function renderHeadTags () {
+    return (
+      <Helmet>
+        <title> Home page of the planets most amazing concerts </title>
+        <meta property="og:title" content="Concerts near you" />
+        <meta property="og:image" content="public/cover-poster.png" />
+      </Helmet>
+    )
+  }
+
   return (
     <div className="home">
+      { renderHeadTags() }
       <h2> Welcome to Home Page! </h2>
       Concert Buddy is an app to help you find concerts
       <br />
