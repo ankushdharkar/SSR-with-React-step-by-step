25e2e22c7d997fde5668bf99cb569408bbb241b8
ankushdharkar <ankushdharkar@gmail.com>
2020-05-15 13:13:46 +0530

    JSX causes error

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const renderToString = require('react-dom/server').renderToString;
+
+const AppComponent = require('./../src/App').default;
+
 
 const app = express();
 
 app.get('**', (req, res) => {
-  res.send('Hello Response');
+  const content = renderToString(<AppComponent />);
+  res.send(content);
 })
 
 
 app.listen(4000, () => {
   console.log('Listening to Port 4000');
 });
-
